580c01dc0fff9c77b26412a09c2d52f6bb85e068
ilkin Balkanay <ilkinulas@gmail.com>
2015-10-11 21:39:09 +0300

    while loop task commit 1
    while loop task commit 2
    
    while loop task commit 3 added comments
    
    squased last 2 commits into the first one. Task done!

--- test.sh
+++ test.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 echo "test"
 
+#c style for loop
 for i in `seq 1 10`;
 do
 	echo $i
 done  
 
+#while loop
+COUNTER=0
+while [  $COUNTER -lt 10 ]; do
+	echo The counter is $COUNTER
+	let COUNTER=COUNTER+1 
+done
+
+#until loop
+COUNTER=20
+until [  $COUNTER -lt 10 ]; do
+	echo COUNTER $COUNTER
+	let COUNTER-=1
+done
 
 echo "done!"
