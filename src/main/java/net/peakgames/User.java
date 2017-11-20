52a49825c39b276a760cba9c36a61cd1e39212cf
ilkin Balkanay <ilkinulas@gmail.com>
2017-11-20 12:08:13 +0300

    user email feature implemented

--- src/main/java/net/peakgames/User.java
+++ src/main/java/net/peakgames/User.java
@@ -2,6 +2,15 @@ package net.peakgames;
 
 public class User {
     private String name;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getName() {
         return name;
